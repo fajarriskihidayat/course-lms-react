cbf6ce546386792930e5eba12907785651f55d6d
fajarriskihidayat <fajarrh2002@gmail.com>
2025-03-06 13:42:23 +0700

    feat: slicing sign in page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
+import ManagerHome from "../pages/ManagerHome";
+import SignInPage from "../pages/SignIn";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <ManagerHome />,
+  },
+  {
+    path: "/manager/sign-in",
+    element: <SignInPage />,
   },
 ]);
 
