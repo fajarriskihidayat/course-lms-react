89c0bbde2f175785c9f3dc9f5a7404caa1521922
fajarriskihidayat <fajarrh2002@gmail.com>
2025-09-02 09:22:16 +0700

    feat: protected route and handle logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import secureLocalStorage from "react-secure-storage";
+import { STORAGE_KEY } from "../utils/const";
 
 const Header = () => {
+  const handleLogout = () => {
+    secureLocalStorage.removeItem(STORAGE_KEY);
+
+    window.location.replace("/manager/sign-in");
+  };
+
   return (
     <div id="TopBar" className="flex items-center justify-between gap-[30px]">
       <form
@@ -51,7 +59,13 @@ const Header = () => {
               <a href="#">Settings</a>
             </li>
             <li className="font-semibold">
-              <a href="signin.html">Logout</a>
+              <button
+                type="button"
+                className="cursor-pointer"
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
             </li>
           </ul>
         </div>
